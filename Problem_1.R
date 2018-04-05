8803f1f729ae5d8b69969ba7fad7ed731278986b
Boyko Amarov <boyko.amarov@gmail.com>
2018-04-05 12:37:58 +0300

    Add review.

--- Problem_1.R
+++ Problem_1.R
@@ -1,7 +1,18 @@
+## NOTE: readability: leave some blank linkes between the code to make it more readable
+
+## NOTE: always leave space before and after <-, -, +, *, /. Leave a blank after commas. Leave blanks around 
+## <, >, ==, <=, =>, etc.
+## Do not leave blanks before paranthesis, e.g.
+
+## Use comments like a), b) to show which part of the problem  
+## you are referring to with your code 
+
 x <- c(4,1,1,4)
 y <- c(1,4)
 z <- x - y
 # R "рециклира" двата вектора, защото са с различна дъжлина, така че да бъде с дължината на по-дългия такъв.
+# Only 'y' gets recycled, because it is the shorter one.
+
 s <- c(x,y)
 rep (s,10)
 length (rep(s,10))
--- Problem_2.R
+++ Problem_2.R
@@ -3,11 +3,26 @@ xmax <- c(25.0 , 22.8 , 31.2 , 27.3 , 28.4 , 20.2 , 24.1)
 difference <- xmax - xmin
 avgmin <- mean(xmin)
 avgmax <- mean(xmax)
+
+## This works but using 'which' is not necessary
 belowavg <- xmin[which (xmin < avgmin)]
-# Тук не съм сигурна дали условието е аналитично на горното или трябва да бъде изпълнено, така както е написано. Затова пиша и двата кода, като намерих логика и в записания в условието, но и в другия. Мога да се опитам да Ви го обясня на упражнението.
-aboveavg <- xmax[which (xmax > avgmax)]
+
+## The following is sufficient
+belowavg <- xmin[xmin < avgmin]
+
+## NOTE: avoid very long lines. Break comments on several lines
+## so that they are readable.
+# Тук не съм сигурна дали условието е аналитично на 
+# горното или трябва да бъде изпълнено, така както е написано. 
+# Затова пиша и двата кода, като намерих логика и в записания в условието, но и в другия. 
+# Мога да се опитам да Ви го обясня на упражнението.
+
+aboveavg <- xmax[which (xmax > avgmax)] ## This is not relevant
 aboveavg2 <- xmin[which (xmax > avgmax)]
+
+
 dayNames <- c('03Mon18' , '04Tue18' , '05Wed18' , '04Thu18' , '05Fri18' , '06Sat18' , '07Sun18')
+## Nice
 names(xmin) <- dayNames
 xmin
 names(xmax) <- dayNames
@@ -19,9 +34,27 @@ temperatures$xminFahrenheit <- temperatures$xmin*(9/5) + 32
 temperatures
 Fahrenheit <- data.frame (temperatures$xminFahrenheit)
 Fahrenheit
-FahrenheitFirst5 <- data.frame (Fahrenheit)
+
+## No need to create another data frame
+FahrenheitFirst5 <- data.frame (Fahrenheit) ## ???
 FahrenheitFirst5 <- data.frame (Fahrenheit = data.frame(temperatures$xminFahrenheit[1:5]))
 FahrenheitFirst5
-FahrenheitLast <- data.frame(Fahrenheit)
+FahrenheitLast <- data.frame(Fahrenheit) ## ???
 FahrenheitLast <- data.frame(Fahrenheit = data.frame(temperatures$xminFahrenheit[-c(6,7)]))
 FahrenheitLast
+
+## Easier way to do this
+
+temperatures <- within(temperatures, {
+  xminFahrenheit <- xmin * (9/5) + 32
+  xmaxFahrenheit <- xmax * (9/5) + 32
+})
+
+temperaturesFahrenheit <- temeratures[, c('xminFahrenheit', 'xmaxFahrenheit)]
+
+## Easier to subset the whole data.frame instead of 
+## doing this for each vector used in its construction
+
+temperaturesFahrenheit[1:5, ]
+temperaturesFahrenheit[-(6:7), ]
+
